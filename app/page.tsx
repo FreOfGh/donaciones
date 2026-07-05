fb505d93cc4b61528725a6c4b54e6f501f4e5817
Simón Torres Saldarriaga <sitorress@unal.edu.co>
2026-07-05 18:04:06 -0500

    fin

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
             {
                 url: process.env.NEXT_PUBLIC_SITE_URL + 'logocma.jpeg',
                 width: 800,
-                height: 600,
+                height: 800,
             },
         
 
