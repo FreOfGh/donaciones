1082f963d1a175b92e25910efe850bcf41def614
Simón Torres Saldarriaga <sitorress@unal.edu.co>
2026-07-05 18:16:36 -0500

    fix tamaño de imagen

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import DonationPhoneForm from '@/app/components/Donation/donationphoneform'
 import ExampleSection from '@/app/components/Donation/examplesection'
 import type { Metadata } from 'next'
 import Hero from '@/app/components/Donation/hero'
+const opengraphImage = process.env.NEXT_PUBLIC_SITE_URL + '/escucdofundacion.jpeg'
 export const metadata: Metadata = {
     title: 'Donation page fundacion Centro Mariana de Alfabetización',
     description: 'Page for donations to support the education of the chilhood in the Esmeralda Town',
@@ -23,7 +24,7 @@ export const metadata: Metadata = {
         siteName: 'Fundacion Centro Mariana de Alfabetización',
         images: 
             {
-                url:  '/escucdofundacion.jpeg',
+                url:  opengraphImage,
                 width: 400,
                 height: 400,
             },
