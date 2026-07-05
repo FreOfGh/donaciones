5e9527327aafd21dac6d0a52131c88076916b7f9
Simón Torres Saldarriaga <sitorress@unal.edu.co>
2026-07-05 18:12:26 -0500

    fix tamaño de imagen

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
         siteName: 'Fundacion Centro Mariana de Alfabetización',
         images: 
             {
-                url: process.env.NEXT_PUBLIC_SITE_URL + 'opengraph.jpeg',
+                url:  '/escucdofundacion.jpeg',
                 width: 400,
                 height: 400,
             },
